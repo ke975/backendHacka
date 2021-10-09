9f0f2772629ccc6cddd30717e0bcb5a572593079
ke975 <60978634+ke975@users.noreply.github.com>
2021-10-08 19:50:35 -0600

    repare el bug del video

--- traveling/src/components/Video.js
+++ traveling/src/components/Video.js
@@ -3,10 +3,10 @@ import Video1 from '../assets/Video1.mp4'
 function Video(){
     return(
         <div>
-<div className="embed-responsive embed-responsive-16by9">
- <video src={Video1} autoplay muted loop ></video>
+        <div className="embed-responsive embed-responsive-16by9">
+<video src={Video1} muted loop autoPlay> </video>
 </div>
-        </div>
+    </div>
     )
 }
 
